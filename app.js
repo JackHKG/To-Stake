ef3e25389d3d735e305d5f64319b0a67c18c9406
JackHKG <lamkwunlong@gmail.com>
2024-06-27 10:46:16 +0800

    Update app.js

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ document.addEventListener("DOMContentLoaded", () => {
         localStorage.setItem('visitCount', visitCount);
         localStorage.setItem('lastVisit', new Date().toISOString());
         updateCounter();
-        window.location.href = 'https://www.stake.com';
+        window.location.href = 'https://stake.com/casino/games/evolution-stake-exclusive-speed-baccarat-1';
     });
 
     updateCounter();
